d0d6d5f1cc01072f0213fa1f57db9bb9ea7b4fcb
RamboXD <rnazymxan@gmail.com>
2023-08-29 22:40:35 +0600

    init

---
+++ constants/images.js
@@ -0,0 +1,5 @@
+import profile from "../assets/images/gojoback.jpg";
+
+export default {
+  profile,
+};
---
+++ hook/useFetch.js
@@ -0,0 +1,40 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const useFetch = (endpoint, query) => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const options = {
+    method: "GET",
+    url: `https://jsearch.p.rapidapi.com/${endpoint}`,
+    params: { ...query },
+    headers: {
+      "X-RapidAPI-Key": "5495e13331msh0835570e9eac4b3p1c1a07jsn746ba1430d1a",
+      "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
+    },
+  };
+
+  const fetchData = async () => {
+    setIsLoading(true);
+    try {
+      const response = await axios.request(options);
+      setData(response.data.data);
+      setIsLoading(false);
+    } catch (error) {
+      setError(error);
+      alert("Bratan tam oshibka");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+  useFetch(() => {
+    fetchData();
+  }, []);
+  const refetch = () => {
+    setIsLoading(true);
+    fetchData();
+  };
+  return { data, isLoading, error, refetch };
+};
