b2950c3cb11c8a313bc685d65e51f339b7b9c70d
RamboXD <rnazymxan@gmail.com>
2023-08-30 22:29:51 +0600

    nearby ready

--- components/common/cards/popular/PopularJobCard.jsx
+++ components/common/cards/popular/PopularJobCard.jsx
@@ -2,11 +2,11 @@ import { View, Text, TouchableOpacity, Image } from "react-native";
 
 import styles from "./popularjobcard.style";
 import { checkImageURL } from "../../../../utils";
-const PopularJobCard = ({ item, selectedJob, handlePress }) => {
+const PopularJobCard = ({ item, selectedJob, handleCardPress }) => {
   return (
     <TouchableOpacity
       style={styles.container(selectedJob, item)}
-      onPress={() => handlePress(item)}
+      onPress={() => handleCardPress(item)}
     >
       <TouchableOpacity style={styles.logoContainer(selectedJob, item)}>
         <Image
